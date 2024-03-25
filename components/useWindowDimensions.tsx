aa030122e340a904760fb47e5cb644eb62ab7b05
Badr <baderalmasre330@gmail.com>
2024-03-25 07:17:02 +0400

    hello

--- components/useWindowDimensions.tsx
+++ components/useWindowDimensions.tsx
@@ -2,18 +2,23 @@ import { useState, useEffect } from 'react';
 
 export default function useWindowDimensions() {
     const hasWindow = typeof window !== 'undefined';
+        var w =0;
+        var h =0;
+      if(hasWindow){
+        const [width, setWidth]   = useState(window.innerWidth);
+        const [height, setHeight] = useState(window.innerHeight);
+        const updateDimensions = () => {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight);
+        }
+        useEffect(() => {
+            window.addEventListener("resize", updateDimensions);
+            return () => window.removeEventListener("resize", updateDimensions);
+        }, []);
+        var w =width;
+        var h =height;
+      }
+  
 
-      
-    const [width, setWidth]   = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
-    const updateDimensions = () => {
-        setWidth(window.innerWidth);
-        setHeight(window.innerHeight);
-    }
-    useEffect(() => {
-        window.addEventListener("resize", updateDimensions);
-        return () => window.removeEventListener("resize", updateDimensions);
-    }, []);
-
-  return hasWindow ?[height, width]:[0,0];
+  return [h, w];
 }
