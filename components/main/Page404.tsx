32f8c197d360f9e46d61bf3a48525b2a5acc7f9f
Badr <baderalmasre330@gmail.com>
2024-03-26 09:45:38 +0400

    hello

--- components/main/Page404.tsx
+++ components/main/Page404.tsx
@@ -6,9 +6,9 @@ import Spline from '@splinetool/react-spline';
 const Page404 = () => {
 
       return (
-        <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-0 z-[20] ">
-            <div className="w-full flex flex-col items-center justify-center m-auto">
-            <Spline   scene="https://prod.spline.design/b6t8O2Ycvz6EalBc/scene.splinecode"   />
+        <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-0 z-[99] ">
+            <div className="w-full flex flex-col items-center justify-center m-auto z-[99] ">
+            <Spline   scene="https://prod.spline.design/b6t8O2Ycvz6EalBc/scene.splinecode z-[99]"   />
             </div>
         </div>
     )
